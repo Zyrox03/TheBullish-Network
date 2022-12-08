f8a5c940c23066a77ebf06ac90830ee6ef900f03
Zyrox <anisanissou2003@gmail.com>
2022-12-08 23:26:17 +0100

    sub headline

--- app.js
+++ app.js
@@ -124,6 +124,7 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 const hiddenElements1 = document.querySelectorAll('.hidden1')
 const hiddenElements2 = document.querySelectorAll('.hidden2')
+const hiddenElements3 = document.querySelectorAll('.hidden3')
 
 
 
@@ -131,6 +132,7 @@ const hiddenElements2 = document.querySelectorAll('.hidden2')
 hiddenElements.forEach((el) => observer.observe(el));
 hiddenElements1.forEach((el) => observer.observe(el));
 hiddenElements2.forEach((el) => observer.observe(el));
+hiddenElements3.forEach((el) => observer.observe(el));
 
 
   // nav on scroll 
