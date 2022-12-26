6aa84e06e38282297123c4dc3f466227c526bb8e
Zyrox <anisanissou2003@gmail.com>
2022-12-26 18:34:47 +0100

    added store section and swiper

--- app.js
+++ app.js
@@ -274,12 +274,44 @@ let getCountMembers = async () => {
 
 
 }
-
-
-
 getCountMembers()
 
 
 
 
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    initialSlide : 1,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+      reverseDirection: false,
+    },
+
+    slidesPerView : 1,
+    spaceBetween :20 ,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+   
+  
